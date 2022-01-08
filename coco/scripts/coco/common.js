25b8dfc279b49a63deff0f2846e646d4ab2e18e4
mjkang0109 <mjkang098765@gmail.com>
2022-01-08 14:27:56 +0900

    feat: 코코수리 메인

---
+++ coco/scripts/coco/common.js
@@ -0,0 +1,119 @@
+"use strict";
+
+(function () {
+  var CONSTANTS = {
+    CLASS: {
+      TOGGLE: 'on',
+      LEFT_NAVIGATION: 'left-navigation'
+    }
+  };
+  var classes = CONSTANTS.CLASS;
+  var toggleClass = classes.TOGGLE,
+      leftNavigation = classes.LEFT_NAVIGATION;
+
+  var visualSwiper = function visualSwiper() {
+    var swiper = new Swiper('.visual-wrapper', {
+      direction: 'horizontal',
+      wrapperClass: 'visual-banners',
+      slideClass: 'swiper',
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: '.visual-pagination',
+        type: 'bullets'
+      },
+      loop: true
+    });
+  };
+
+  var fixSwiper = function fixSwiper() {
+    var swiper = new Swiper('.fix-item-wrapper', {
+      direction: 'horizontal',
+      wrapperClass: 'fix-items',
+      slideClass: 'item',
+      slidesPerView: 'auto'
+    });
+  };
+
+  var bindLeftCategory = function bindLeftCategory(_ref) {
+    var trigger = _ref.trigger,
+        el = _ref.el;
+
+    if (!trigger) {
+      return;
+    }
+
+    if (!el) {
+      return;
+    }
+
+    var btnClose = el.querySelectorAll('.btn-close');
+
+    var close = function close() {
+      el.classList.remove(toggleClass);
+      btnClose.forEach(function (btn) {
+        btn.removeEventListener('click', close);
+      });
+      trigger.addEventListener('click', open);
+    };
+
+    var open = function open() {
+      el.classList.add(toggleClass);
+      trigger.removeEventListener('click', open);
+      btnClose.forEach(function (btn) {
+        btn.addEventListener('click', close);
+      });
+    };
+
+    var windowHandler = function windowHandler(e) {
+      if (e.target.className.indexOf(leftNavigation) > -1) {
+        close();
+      }
+    };
+
+    trigger.addEventListener('click', open);
+    el.addEventListener('click', windowHandler);
+  };
+
+  var bindTab = function bindTab() {
+    var tabs = document.querySelectorAll('[role="tab"]');
+
+    var changeTabs = function changeTabs(e) {
+      var target = e.target;
+      var parent = target.parentNode;
+      var grandparent = parent.parentNode;
+      parent.querySelectorAll('[aria-selected="true"]').forEach(function (t) {
+        t.classList.remove(toggleClass);
+        t.setAttribute('aria-selected', false);
+      });
+      target.setAttribute('aria-selected', true);
+      target.classList.add(toggleClass);
+      grandparent.querySelectorAll('[role="tabpanel"]').forEach(function (p) {
+        p.classList.remove(toggleClass);
+        p.setAttribute('hidden', true);
+      });
+      grandparent.parentNode.querySelector("#".concat(target.getAttribute('aria-controls'))).removeAttribute('hidden');
+      grandparent.parentNode.querySelector("#".concat(target.getAttribute('aria-controls'))).classList.add(toggleClass);
+    };
+
+    tabs.forEach(function (tab) {
+      tab.addEventListener('click', changeTabs);
+    });
+  };
+
+  var onInit = function onInit() {
+    visualSwiper();
+    fixSwiper();
+    bindLeftCategory({
+      trigger: document.querySelector('header .btn-category'),
+      el: document.querySelector('.left-navigation')
+    });
+    bindTab();
+  };
+
+  if (document.readyState === 'complete') {
+    onInit();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', onInit);
+  }
+})();

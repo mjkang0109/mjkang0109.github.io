9b95a60dc096029a696a4502f5ba2ecec0321dea
mjkang0109 <mjkang098765@gmail.com>
2022-01-17 12:04:32 +0900

    refacotr: 폴더정리
    * 상품정보
      * 상품후기 작성 팝업
      * 상품문의 작성 팝업
    * 장바구니
    * 주문/결제
    * 주문완료
    * 날짜선택 팝업

---
+++ cokco/mobile/scripts/cokco/common.js
@@ -0,0 +1,335 @@
+"use strict";
+
+(function () {
+  var CONSTANTS = {
+    CLASS: {
+      TOGGLE: 'on',
+      LEFT_NAVIGATION: 'left-navigation',
+      IS_SEARCH: 'is-search',
+      DEPTH_CATEGORY: 'category-depth',
+      SORT_ITEM: 'sort-item'
+    }
+  };
+  var classes = CONSTANTS.CLASS;
+  var toggleClass = classes.TOGGLE,
+      leftNavigation = classes.LEFT_NAVIGATION,
+      isSearch = classes.IS_SEARCH,
+      depthCategory = classes.DEPTH_CATEGORY,
+      sortItem = classes.SORT_ITEM;
+
+  var visualSwiper = function visualSwiper() {
+    var swiper = new Swiper('.visual-wrapper', {
+      direction: 'horizontal',
+      wrapperClass: 'visual-banners',
+      slideClass: 'swiper',
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: '.visual-pagination',
+        type: 'bullets'
+      },
+      loop: true
+    });
+  };
+
+  var fixSwiper = function fixSwiper() {
+    var swiper = new Swiper('.swiper-item-wrapper', {
+      direction: 'horizontal',
+      wrapperClass: 'swiper-items',
+      slideClass: 'swiper-item',
+      slidesPerView: 'auto'
+    });
+  };
+
+  var storeCategoriesSwiper = function storeCategoriesSwiper() {
+    var swiper = new Swiper('.store-categories-wrap', {
+      direction: 'horizontal',
+      wrapperClass: 'store-categories',
+      slideClass: 'store-category-item',
+      slidesPerView: 'auto'
+    });
+  };
+
+  var thumbsSwiper = function thumbsSwiper() {
+    var thumbs = new Swiper('.thumbs-wrapper', {
+      wrapperClass: 'thumbs-items',
+      slideClass: 'thumbs-item',
+      slidesPerView: 'auto',
+      watchSlidesProgress: true
+    });
+    var gallery = new Swiper('.gallery-wrapper', {
+      wrapperClass: 'gallery-items',
+      slideClass: 'gallery-item',
+      loop: true,
+      slidesPerView: 'auto',
+      thumbs: {
+        swiper: thumbs
+      }
+    });
+  };
+
+  var bindCategory = function bindCategory(e) {
+    if (!e) {
+      return document.querySelectorAll(".".concat(depthCategory, "-1 > li.on")).forEach(function (el) {
+        return el.classList.remove(toggleClass);
+      });
+    }
+
+    if (!e.target.parentNode.querySelector(".".concat(depthCategory, "-2"))) {
+      return;
+    }
+
+    if (e.target.tagName !== 'A') {
+      return;
+    }
+
+    e.preventDefault();
+
+    if (e.target.parentElement.getAttribute('class')) {
+      return e.target.parentElement.classList.remove(toggleClass);
+    }
+
+    if (e.target.parentNode.parentNode.querySelector(".".concat(toggleClass))) {
+      e.target.parentNode.parentNode.querySelector(".".concat(toggleClass)).classList.remove(toggleClass);
+    }
+
+    e.target.parentElement.classList.add(toggleClass);
+  };
+
+  var bindLeftCategory = function bindLeftCategory(_ref) {
+    var trigger = _ref.trigger,
+        el = _ref.el;
+
+    if (!trigger) {
+      return;
+    }
+
+    if (!el) {
+      return;
+    }
+
+    var btnClose = el.querySelectorAll('.btn-close');
+
+    var close = function close() {
+      el.classList.remove(toggleClass);
+      btnClose.forEach(function (btn) {
+        btn.removeEventListener('click', close);
+      });
+      trigger.addEventListener('click', open);
+      el.removeEventListener('click', bindCategory);
+    };
+
+    var open = function open() {
+      el.classList.add(toggleClass);
+      trigger.removeEventListener('click', open);
+      btnClose.forEach(function (btn) {
+        btn.addEventListener('click', close);
+      });
+      el.addEventListener('click', bindCategory);
+    };
+
+    var windowHandler = function windowHandler(e) {
+      if (e.target.className.indexOf(leftNavigation) > -1) {
+        close();
+      }
+    };
+
+    trigger.addEventListener('click', open);
+    el.addEventListener('click', windowHandler);
+    el.addEventListener('click', bindCategory);
+  };
+
+  var bindSearch = function bindSearch(_ref2) {
+    var trigger = _ref2.trigger,
+        el = _ref2.el;
+
+    if (!trigger) {
+      return;
+    }
+
+    if (!el) {
+      return;
+    }
+
+    var setSearch = function setSearch() {
+      trigger.parentNode.classList.add(isSearch);
+      el.focus();
+      window.addEventListener('click', windowHandler);
+    };
+
+    var hideSearch = function hideSearch() {
+      trigger.parentNode.classList.remove(isSearch);
+      window.removeEventListener('click', windowHandler);
+    };
+
+    var windowHandler = function windowHandler(e) {
+      e.preventDefault();
+      var returnClasses = ['btn-search', 'input-search', 'btn-toggle-search'];
+
+      if (returnClasses.indexOf(e.target.className) === -1) {
+        return hideSearch();
+      }
+    };
+
+    trigger.addEventListener('click', setSearch);
+  };
+
+  var bindTab = function bindTab() {
+    var tabs = document.querySelectorAll('[role="tab"]');
+
+    var changeTabs = function changeTabs(e) {
+      var target = e.target;
+      var parent = target.parentNode;
+      var grandparent = parent.parentNode;
+      parent.querySelectorAll('[aria-selected="true"]').forEach(function (t) {
+        t.classList.remove(toggleClass);
+        t.setAttribute('aria-selected', false);
+      });
+      target.setAttribute('aria-selected', true);
+      target.classList.add(toggleClass);
+      grandparent.querySelectorAll('[role="tabpanel"]').forEach(function (p) {
+        p.classList.remove(toggleClass);
+        p.setAttribute('hidden', true);
+      });
+
+      if (target.getAttribute('aria-controls') === 'panel-all') {
+        return grandparent.parentNode.querySelectorAll('.panels [role="tabpanel"]').forEach(function (p) {
+          p.classList.add(toggleClass);
+          p.removeAttribute('hidden');
+        });
+      }
+
+      grandparent.parentNode.querySelector("#".concat(target.getAttribute('aria-controls'))).removeAttribute('hidden');
+      grandparent.parentNode.querySelector("#".concat(target.getAttribute('aria-controls'))).classList.add(toggleClass);
+      bindCategory();
+    };
+
+    tabs.forEach(function (tab) {
+      tab.addEventListener('click', changeTabs);
+    });
+  };
+
+  var bindRadio = function bindRadio() {
+    var radios = document.querySelectorAll('.radio-tabs input[type="radio"]');
+
+    var changeRadios = function changeRadios(e) {
+      var target = e.target;
+      var parent = target.parentNode.parentElement;
+      var grandparent = parent.parentNode;
+      grandparent.querySelectorAll('.radio-panels > div').forEach(function (p) {
+        p.classList.remove(toggleClass);
+        p.setAttribute('hidden', true);
+      });
+      grandparent.parentNode.querySelector("#".concat(target.getAttribute('aria-controls'))).removeAttribute('hidden');
+      grandparent.parentNode.querySelector("#".concat(target.getAttribute('aria-controls'))).classList.add(toggleClass);
+    };
+
+    radios.forEach(function (radio) {
+      radio.addEventListener('click', changeRadios);
+    });
+  };
+
+  var bindDropdown = function bindDropdown() {
+    var close = function close(_ref3) {
+      var el = _ref3.el;
+      el.classList.remove(toggleClass);
+    };
+
+    var setSelected = function setSelected(e) {
+      e.target.parentElement.querySelectorAll('button.on').forEach(function (b) {
+        b.classList.remove(toggleClass);
+      });
+      e.target.classList.add(toggleClass);
+      e.target.parentElement.parentElement.querySelector('.btn-current span').textContent = e.target.querySelector('span').textContent;
+      close({
+        el: e.target.parentElement.parentElement
+      });
+    };
+
+    var toggleHandler = function toggleHandler(e) {
+      if (e.target.parentElement.className.indexOf(' on') > -1) {
+        return close({
+          el: e.target.parentElement
+        });
+      }
+
+      if (e.target.parentElement.className.indexOf('dropdown-items') > -1) {
+        e.target.parentElement.classList.add(toggleClass);
+      }
+
+      e.target.parentNode.querySelectorAll(".".concat(sortItem)).forEach(function (s) {
+        s.addEventListener('click', setSelected);
+      });
+    };
+
+    document.querySelectorAll('.dropdown-items').forEach(function (d) {
+      d.addEventListener('click', toggleHandler);
+    });
+  };
+
+  var setQuantity = function setQuantity(_ref4) {
+    var el = _ref4.el;
+
+    if (el.length === 0) {
+      return;
+    }
+
+    var handlerAmount = function handlerAmount(_ref5) {
+      var amount = _ref5.amount,
+          type = _ref5.type;
+
+      if (!amount) {
+        return;
+      }
+
+      if (type === 'decrease' && Number(amount.textContent) === 0) {
+        return;
+      }
+
+      amount.textContent = Number(amount.textContent - (type === 'increase' ? -1 : +1));
+    };
+
+    el.forEach(function (element) {
+      var amountEl = element.querySelector('.amount');
+      element.querySelector('.btn-increase').addEventListener('click', function () {
+        handlerAmount({
+          amount: amountEl,
+          type: 'increase'
+        });
+      });
+      element.querySelector('.btn-decrease').addEventListener('click', function () {
+        handlerAmount({
+          amount: amountEl,
+          type: 'decrease'
+        });
+      });
+    });
+  };
+
+  var onInit = function onInit() {
+    visualSwiper();
+    storeCategoriesSwiper();
+    fixSwiper();
+    thumbsSwiper();
+    bindLeftCategory({
+      trigger: document.querySelector('header .btn-category'),
+      el: document.querySelector('.left-navigation')
+    });
+    bindSearch({
+      trigger: document.querySelector('header .btn-toggle-search'),
+      el: document.querySelector('header .input-search')
+    });
+    bindTab();
+    bindRadio();
+    bindDropdown();
+    setQuantity({
+      el: document.querySelectorAll('.quantity')
+    });
+  };
+
+  if (document.readyState === 'complete') {
+    onInit();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', onInit);
+  }
+})();

cc7477fd28b27b4cf4e656726c10420dfcaa65fe
kang.jiwoo <kang.minjeong2@joongang.co.kr>
2025-12-19 14:52:23 +0900

    feat: wonju

--- client/src/scripts/wonju/scripts.js
+++ client/src/scripts/wonju/scripts.js
@@ -1,4 +1,19 @@
 const JS = (() => {
+
+    const throttling = (callback = () => {
+    }, timing = 100) => {
+        let timer;
+
+        return (...args) => {
+            if (!timer) {
+                timer = setTimeout(() => {
+                    callback(...args);
+                    timer = null;
+                }, timing);
+            }
+        };
+    };
+
     const bindSwiper = () => {
         const swiperThumb = new Swiper('.main-swiper .swiper-thumb', {
             loop               : true,
@@ -24,7 +39,44 @@ const JS = (() => {
             wrapper.dataset.index = swiper.realIndex + 1;
         });
 
+        const swiperFooter = new Swiper('#footer .swiper-footer', {
+            navigation   : {
+                nextEl: '#footer .button-next',
+                prevEl: '#footer .button-prev',
+            },
+            breakpoints: {
+                0   : {
+                    slidesPerView: 2.8,
+                    spaceBetween : 20,
+                },
+                1200: {
+                    slidesPerView: 5,
+                    spaceBetween : 10,
+                },
+            }
+        });
+
+        const swiperQuick = new Swiper('.quick-nav .swiper-quick', {
+            breakpoints: {
+                0   : {
+                    slidesPerView : 1.5,
+                    spaceBetween  : 20,
+                    centeredSlides: true,
+                },
+                1200: {
+                    spaceBetween  : 0,
+                    slidesPerView : 'auto',
+                    centeredSlides: false,
+                },
+            },
+            navigation : {
+                nextEl: '.quick-nav .button-next',
+                prevEl: '.quick-nav .button-prev',
+            },
+        });
     };
+
+    window.addEventListener('resize', throttling(bindSwiper, 50));
 
     const init = () => {
         bindSwiper();
